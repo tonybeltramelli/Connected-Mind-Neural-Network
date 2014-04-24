95ea4e6526c7c7e512cc46425f043c9eb8d4a5f7
Tony Beltramelli <tbeltramelli@gmail.com>
2014-04-25 00:42:34 +0200

    update regular expression

--- src/com/tonybeltramelli/lib/neural/Encodable.java
+++ src/com/tonybeltramelli/lib/neural/Encodable.java
@@ -10,9 +10,9 @@ public interface Encodable
     public static final char INPUT = 'i';
     public static final char WEIGHT = 'w';
     //
-    public static final String DNA_REG_EXP = "([iho][0-9]+([w][0-9]+[ho][0-9]+))([w][0-9]+[ho][0-9]+)*";
-    public static final String CONNECTION_REG_EXP = "([w][0-9]+[ho][0-9]+)";
-    public static final String WEIGHT_REG_EXP = "([who][0-9]+)";
+    public static final String DNA_REG_EXP = "(["+INPUT+""+HIDDEN+""+OUTPUT+"][0-9]+(["+WEIGHT+"][0-9]+["+HIDDEN+""+OUTPUT+"][0-9]+))(["+WEIGHT+"][0-9]+["+HIDDEN+""+OUTPUT+"][0-9]+)*";
+    public static final String CONNECTION_REG_EXP = "(["+WEIGHT+"][0-9]+["+HIDDEN+""+OUTPUT+"][0-9]+)";
+    public static final String WEIGHT_REG_EXP = "(["+WEIGHT+""+HIDDEN+""+OUTPUT+"][0-9]+)";
     //
     public String getEncoding();
 }
